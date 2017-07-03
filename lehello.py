69f4bba7180f4ead8df18d40a0524c0f2b899bae
Cristóbal Ganter <cganterh@gmail.com>
2017-07-02 22:53:43 -0400

    Added documentation to lehello.py

--- lehello.py
+++ lehello.py
@@ -1,7 +1,10 @@
+"""Provide a simple greeting handler for a Le bot."""
+
 from telegram.ext import CommandHandler
 
 
 def hello(bot, update):
+    """Greet the user using own name."""
     bot.sendMessage(
         chat_id=update.message.chat_id,
         text="Hi all, I'm {bot.first_name}.".format(bot=bot)
